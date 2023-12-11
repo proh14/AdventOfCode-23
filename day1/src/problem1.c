f5a4085ea31b2ae4c2049b266ee67ebd0fa7cce0
Hoorad <hourrad.f@gmail.com>
2023-12-11 16:21:43 +1300

    Better code style for problem1.c

--- day1/src/problem1.c
+++ day1/src/problem1.c
@@ -4,35 +4,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int findFirstDigit(char *s, int len) {
+  int i;
+  for (i = 0; i < len; i++) {
+    if (isdigit(s[i])) {
+      return s[i] - '0';
+    }
+  }
+  return -1;
+}
+
+int findLastDigit(char *s, int len) {
+  int i;
+  int max = -1;
+  for (i = 0; i < len; i++) {
+    if (isdigit(s[i]) && i > max) {
+      max = i;
+    }
+  }
+  return s[max] - '0';
+}
+
 int main() {
   FILE *fp = fopen("../res/input.txt", "r");
-  int n = 0, sum = 0;
+  int sum = 0, len = 0;
   size_t size = 0;
   char *s;
-  int len;
-  int i;
-  int max = 0;
+
   while ((len = getline(&s, &size, fp)) != -1) {
-    for (i = 0; i < len; i++) {
-      if (isdigit(s[i])) {
-        n = n * 10 + (s[i] - '0');
-        break;
-      }
-    }
-    for (; i < len; i++) {
-      if (isdigit(s[i])) {
-        if (i > max) {
-          max = i;
-        }
-      }
-    }
-    n = n * 10 + (s[max] - '0');
-    sum += n;
-    n = 0;
-    max = 0;
+    int firstDigit = findFirstDigit(s, len);
+
+    int lastDigit = findLastDigit(s, len);
+    sum += firstDigit * 10 + lastDigit;
   }
 
   printf("sum = %d\n", sum);
+  free(s);
   fclose(fp);
   return 0;
 }
